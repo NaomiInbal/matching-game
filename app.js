d81e532c16d5d90821bb1eb17f12b9f513abfd63
Naomi <naominbal@gmail.com>
2022-09-01 21:12:11 +0300

    set time out

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 let score = 0;
 let scoreVal = document.getElementsByClassName("score-value")[0];
+let isCurrentCardsEqual;
+let didUserCoose = false;
+let userChoiceTimeOut;
 
 function animateWelcomeScreen() {
     const colors = ["red", "violet", "green", "yellow", "blue", "purple"];
@@ -13,14 +16,11 @@ function animateWelcomeScreen() {
         if (colorIndex > colors.length - 1) {
             clearInterval(intervalId);
             setTimeout(() => {
-                element.style.display = "none";
-                animateProgressLoader();
-            }, 2000);
-        }
+                element.style.display = "none";                
+            }, 2000);        }
     }, 500);
 }
-animateWelcomeScreen();
-function animateProgressLoader() {
+function animateLoaderCircle() {
     const loaderElement = document.getElementsByClassName("loader")[0];
     loaderElement.style.display = "block";
     const loaderPercentsElement =
@@ -34,14 +34,7 @@ function animateProgressLoader() {
                 const loaderWrapperElement =
                     document.getElementsByClassName("loader-wrapper")[0];
                 loaderWrapperElement.style.display = "none";
-                animateCardsBoxes();
-                setTimeout(() => {
-                    showInstruction();
-                }, 1000);
-                setTimeout(() => {
-                    play();
-                }, 7000);
-            }, 3000);
+            }, 1500);
         }
         percents++;
     }, 25);
@@ -55,63 +48,87 @@ function animateCardsBoxes() {
     setTimeout(() => {
         card1.style.transform = "translateX(-35vw)";
         card2.style.transform = "translateX(35vw)";
-    });
+    },100);
 }
 function showInstruction() {
-    var instructionWrapper = document.getElementsByClassName(
-        "instruction-wrapper"
-    )[0];
-    instructionWrapper.style.display = "flex";
+    const canvas = document.getElementById("game-instructions");
+    canvas.style.display = "block";
     setTimeout(() => {
-        instructionWrapper.style.display = "none";
-    }, 5000);
-    var canvas = document.getElementById("game-instructions");
-    var ctx = canvas.getContext("2d");
-    ctx.font = "12px Arial";
-    ctx.fillStyle = "red";
-    ctx.textAlign = "center";
-    ctx.fillText("הוראות המשחק: \n אם הבחירה ");
+        canvas.style.display = "none";
+    }, 5000);   
+    const ctx = canvas.getContext("2d");    
+    ctx.font = "20px Arial";
+    ctx.textAlign = "right";
+    ctx.fillText('הוראות המשחק:', 580, 50);
+    ctx.fillText('עליך לענות האם הכרטיסים שווים או לא.', 580, 80);
+    ctx.fillText('על תשובה נכונה תזכה ב5 נקודות.', 580, 110);
+    ctx.fillText('תשובה שגויה תחסיר לך חמש נקודות.', 580, 140);
+    ctx.fillText('הימנעות מתשובה עד סוף הזמן המוקצב אינה מעלה או מורידה נקודות.', 580, 170);
+    ctx.fillText('בהצלחה!', 580, 200);
+
 }
 function animateProgressBar() {
     const myBarElement = document.getElementById("my-bar");
-    setTimeout(() => {
+    myBarElement.style.transition = 'none';
+    myBarElement.style.width = 0;
+
+    setTimeout(() =>{
+        myBarElement.style.transition = 'width 10s linear';
         myBarElement.style.width = "100%";
-    });
+    },500)
+
+
 }
-function play() {
-    animateProgressBar();
-    let randomNumber1 = Math.floor(Math.random() * 13) + 1;
-    let randomNumber2 = Math.floor(Math.random() * 13) + 1;
-    let isEqual = randomNumber1 === randomNumber2;
-    cardImg1 = document.getElementsByClassName("card-img-1")[0];
-    cardImg2 = document.getElementsByClassName("card-img-2")[0];
-    cardImg1.src = "./assets/card" + randomNumber1 + ".jpg";
-    cardImg2.src = "./assets/card" + randomNumber2 + ".jpg";
+
+function listenToUserChoice(){
     btnV = document.getElementsByClassName("v-btn")[0];
     btnX = document.getElementsByClassName("x-btn")[0];
-    btnX.disabled = false;
-    btnV.disabled = false;
     btnV.addEventListener("click", () => {
+        didUserCoose = true;
         btnV.disabled = true;
         btnX.disabled = true;
-        if (isEqual) {
+        if (isCurrentCardsEqual) {
             onUserSuccess();
         } else {
             oUuserFailure();
         }
     });
     btnX.addEventListener("click", () => {
+        didUserCoose = true;
         btnX.disabled = true;
         btnV.disabled = true;
-        if (isEqual) {
+        if (isCurrentCardsEqual) {
             oUuserFailure();
         } else {
             onUserSuccess();
         }
     });
+    
+}
+
+function play() {
+    didUserCoose = false;
+    animateProgressBar();
+    btnV = document.getElementsByClassName("v-btn")[0];
+    btnX = document.getElementsByClassName("x-btn")[0];
+    btnX.disabled = false;
+    btnV.disabled = false;
+    let randomNumber1 = Math.floor(Math.random() * 13) + 1;
+    let randomNumber2 = Math.floor(Math.random() * 13) + 1;
+    isCurrentCardsEqual = randomNumber1 === randomNumber2;
+    cardImg1 = document.getElementsByClassName("card-img-1")[0];
+    cardImg2 = document.getElementsByClassName("card-img-2")[0];
+    cardImg1.src = "./assets/card" + randomNumber1 + ".jpg";
+    cardImg2.src = "./assets/card" + randomNumber2 + ".jpg";
+    userChoiceTimeOut = setTimeout(() =>{
+        clearTimeout(userChoiceTimeOut)
+        if(!didUserCoose){
+            play();
+        }
+    },10500)
 }
 function onUserSuccess() {
-    score += 5;
+    score = score +5;
     scoreVal.innerHTML = score;
     let cardBorder1 = document.getElementsByClassName("card-box-1")[0];
     let cardBorder2 = document.getElementsByClassName("card-box-2")[0];
@@ -121,6 +138,7 @@ function onUserSuccess() {
     setTimeout(() => {
         cardBorder1.style.borderColor = "black";
         cardBorder2.style.borderColor = "black";
+        play();
     }, 300);
 }
 function oUuserFailure() {
@@ -133,8 +151,24 @@ function oUuserFailure() {
     setTimeout(() => {
         cardBorder1.style.borderColor = "black";
         cardBorder2.style.borderColor = "black";
+        play();
     }, 300);
 }
 
-// animateCardsBoxes();
-// animateProgressBar();
+function main(){
+    animateWelcomeScreen();
+    setTimeout(() => {
+        animateLoaderCircle();
+    },5000)
+    setTimeout(() =>{
+        animateCardsBoxes();
+    },9000);
+    setTimeout(() =>{
+        showInstruction();
+    },10500)
+    setTimeout(() =>{
+        listenToUserChoice();
+        play();
+    },15500)
+}
+main();
